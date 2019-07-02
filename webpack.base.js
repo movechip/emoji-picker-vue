d1ac8ddbd750ea815989780286d7ffa77079cd21
chip <chip@chip.com>
2019-07-02 23:32:27 +0800

    origin

---
+++ webpack.base.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        index: path.join(__dirname, "/src/index.js")
+    },
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "[name].js"
+    },
+    resolve: {
+        alias: {
+            "vue$": "vue/dist/vue.js"
+        },
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.styl(us)?/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                        // you can specify a publicPath here
+                        // by default it uses publicPath in webpackOptions.output
+                        hmr: process.env.NODE_ENV === 'development',
+                        },
+                    }
+                    ,"css-loader","stylus-loader"
+                ]
+            },
+            {
+                test: /\.pug$/,
+                oneOf: [
+                    // this applies to `<template lang="pug">` in Vue components
+                    {
+                        resourceQuery: /^\?vue/,
+                        use: ['pug-plain-loader']
+                    },
+                    // this applies to pug imports inside JavaScript
+                    {
+                        use: ['raw-loader', 'pug-plain-loader']
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname,'/src/template/index.template.html')
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    name: "common",
+                    chunks: "initial", 
+                    minChunks: 1
+                }
+            }
+        }
+    }
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const webpackBaseConfig = require('./webpack.base.js');
+console.log(process.env.NODE_ENV);
+module.exports = merge(webpackBaseConfig,{
+    mode: "development",
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: "./dist", //本地服务器所加载的页面所在的目录
+        port: "8088",
+        historyApiFallback: true, //不跳转
+        inline: true,
+        hot: true,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8080',
+                pathRewrite: {'/api':''},
+                changeOrigin: true,
+                secure: false
+            }
+        }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+})
